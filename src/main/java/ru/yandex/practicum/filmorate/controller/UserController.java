39693de78113f65ddcd0bad5a6f4b325fd8bbbc7
DitrikhAnastasia <ditrihanastasya@yandex.ru>
2023-03-21 22:49:06 +0300

    fix: исправить pull-request
    Future: Создать API согласно ТЗ

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,88 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+
+import java.time.LocalDate;
+import java.util.*;
+
+
+@RequestMapping("/users")
+@RestController
+@Slf4j
+@Data
+public class UserController {
+
+    protected final Map<Integer, User> users = new HashMap<>();
+    protected Integer idUserGen = 1;
+
+
+    @GetMapping
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @PostMapping
+    public User add(@RequestBody User user) { //
+        validateUser(user);
+        for (User value : users.values()) {
+            if (user.getLogin().equals(value.getLogin())) {
+                throw new ValidationException("Пользователь с таким логином зарегистрирован");
+            }
+        }
+        user.setId(idUserGen);
+        users.put(idUserGen, user);
+        idUserGen++;
+        log.info("Пользователь с id = {} успешно добален", user.getId());
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user) {
+        validateUser(user);
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            log.info("Пользователь с id = {} успешно обновлен", user.getId());
+        } else {
+            log.warn("Пользователь с id {} не обновлен, т.к. не зарегистрирован", user.getId());
+            throw new ValidationException("Невозможно обновить данные пользователя. Такого пользователя не существует");
+        }
+        return user;
+    }
+
+    public static void validateUser(User user) throws ValidationException {
+        String mail = user.getEmail();
+        if (mail == null || mail.isEmpty() || mail.isBlank()) {
+            log.warn("Пользователь с id = {} не указал электронную почту", user.getId());
+            throw new ValidationException("Электронная почта не указана");
+        } else if (!mail.contains("@")) {
+            log.warn("Электронная почта для пользователя с id = {} указана некорректно", user.getId());
+            throw new ValidationException("Электронная почта указана некорректно");
+        } else if (user.getLogin() == null || user.getLogin().contains(" ") || user.getLogin().isBlank()) {
+            log.warn("Пользователь с id = {} не указал логин", user.getId());
+            throw new ValidationException("Поле логин не может быть пустым или содержать пробелы");
+        } else if (user.getName() == null || user.getName().isBlank() || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+            log.info("В качестве имени пользователя с id = {} будет использоваться логин", user.getId());
+        } else if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Пользователь с id = {} некорректно указал дату рождения", user.getId());
+            throw new ValidationException("Неккоректно введена дата рождения");
+        }
+        log.info("Пользователь с id = {} успешно прошел валидацию", user.getId());
+    }
+}
+
+
+
+
+
+
+
+
+
+
